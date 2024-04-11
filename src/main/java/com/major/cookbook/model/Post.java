b2c563c73e54fc7320f1b6137f55209df0c7ba6a
nomander17 <nomander17@gmail.com>
2024-04-11 19:42:24 +0530

    userID User type -> Integer fix

--- src/main/java/com/major/cookbook/model/Post.java
+++ src/main/java/com/major/cookbook/model/Post.java
@@ -28,7 +28,7 @@ public class Post {
 	
 	@ManyToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="userID")
-	private User userID;
+	private Integer userID;
 	
 	@Column(name="time")
 	@Temporal(TemporalType.TIMESTAMP)
@@ -58,7 +58,7 @@ public class Post {
 		this.postID = postID;
 	}
 
-	public User getUserID() {
+	public Integer getUserID() {
 		return userID;
 	}
 

af4c06df513b9a8ca272622f240e3e05be73c032
Shamik <shamikghosh678@gmail.com>
2024-04-21 01:44:46 +0530

    Removed extra getters and setters

--- src/main/java/com/major/cookbook/model/User.java
+++ src/main/java/com/major/cookbook/model/User.java
@@ -87,30 +87,6 @@ public class User {
 		this.email = email;
 		this.password = password;
 	}
-	public Integer getId() {
-		return userID;
-	}
-	public void setId(Integer userID) {
-		this.userID = userID;
-	}
-	public String getName() {
-		return userName;
-	}
-	public void setName(String userName) {
-		this.userName = userName;
-	}	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}	
 	@Override
 	public String toString() {
 		return "User [userID=" + userID + ", userName=" + userName + ", email=" + email + ", password=" + password + "]";

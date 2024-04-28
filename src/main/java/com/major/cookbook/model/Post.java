5080dc74a4bcf7f3a158944c9ebd41b0b81e94d4
Shamik <shamikghosh678@gmail.com>
2024-04-29 01:13:33 +0530

    Added @JsonIgnore

--- src/main/java/com/major/cookbook/model/Post.java
+++ src/main/java/com/major/cookbook/model/Post.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -37,10 +39,12 @@ public class Post {
     @Lob
     @Column(name = "image", columnDefinition = "BLOB")
     private byte[] image;
-
+    
+    @JsonIgnore
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Like> likes = new ArrayList<>();
 
+    @JsonIgnore
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 

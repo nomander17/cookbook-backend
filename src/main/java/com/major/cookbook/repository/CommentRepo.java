695096117b1541713d728c966ba1590a3c778f27
Shamik <shamikghosh678@gmail.com>
2024-04-29 01:14:28 +0530

    Added a few Queries in Repo

--- src/main/java/com/major/cookbook/repository/CommentRepo.java
+++ src/main/java/com/major/cookbook/repository/CommentRepo.java
@@ -1,9 +1,17 @@
 package com.major.cookbook.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.major.cookbook.model.Comment;
+import com.major.cookbook.model.Post;
 
 public interface CommentRepo extends JpaRepository<Comment, Integer>{
+
+	public List<Comment> findByPost(Optional<Post> post);
+
+	public Optional<Comment> findByPostAndCommentId(Optional<Post> post, int commentID);
 
 }

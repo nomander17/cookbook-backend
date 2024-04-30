7428967723d8313637bd8732aed3be1a727c7e14
Shamik <shamikghosh678@gmail.com>
2024-05-01 05:27:10 +0530

    Added getLikesByPostId() & getLikesByCommentId()

--- src/main/java/com/major/cookbook/services/LikeService.java
+++ src/main/java/com/major/cookbook/services/LikeService.java
@@ -13,26 +13,20 @@ public interface LikeService {
 	
 	//To Add new like to Comment
 	public Like addCommentLike(Like like);
-	
-	//Checking if a post is already liked
-	@Query("SELECT i FROM Like i WHERE i.user.id=:userId AND i.post.id=:postId")
-	public Like alreadyLikedPost(@Param("userId")int userId,@Param("postId")int postId);
-	
-	//Checking if a comment is already liked
-	@Query("SELECT i from Like i WHERE i.user.id=:userId AND i.comment.id=:commentId")
-	public Like alreadyLikedComment(@Param("userId")int userId, @Param("commenrtId")int commentId);
-	
-	//To get all likes from posts
-	@Query("SELECT i FROM Like i WHERE i.post.id=:postId")
-	public List<Like> getPostLike(@Param("postId") int postId);
-	
-	//To get all likes from Comment
-	@Query("SELECT i FROM Like i WHERE i.comment.id=:commentId")
-	public List<Like> getCommentLike(@Param("commentId") int commentId);
 
+	//GET all likes
     public List<Like> getAllLikes();
+    
+    //GET all likes by post ID
+    public List<Like> getLikesByPostId(int postId);
 
-    public void deleteLikeById(int int1);
+    //GET all likes by comment ID
+	public List<Like> getLikesByCommentId(int commentId);
 
+    //DELETE like by Like ID
+    public Like deleteLikeById(int int1);
+
+    //Update like
     public void updateLike(Like like);
+
 }

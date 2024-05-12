d051310e1d47e6d34b5fb8dc98421c2cf7735bd6
Shamik <shamikghosh678@gmail.com>
2024-05-13 01:50:33 +0530

    Configured which endpoints accessible by which role

--- src/main/java/com/major/cookbook/config/SecurityConfig.java
+++ src/main/java/com/major/cookbook/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.major.cookbook.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +20,7 @@ import com.major.cookbook.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
 
 	@Autowired
     private JwtAuthenticationEntryPoint point;
@@ -36,6 +39,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(
             		auth->auth
             		.requestMatchers("/api/auth/login").permitAll().requestMatchers("/api/auth/register").permitAll()
+            		.requestMatchers("/api/admin/**").hasRole("ADMIN")
+            		.requestMatchers("/api/**").hasRole("USER")
             		.anyRequest().authenticated())
             .exceptionHandling(ex->ex.authenticationEntryPoint(point))
             .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//Stateless since not stored in server

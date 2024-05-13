d62fdf7ead842e9c2e5d8331dee23e4c41e891af
Noman <nomander17@gmail.com>
2024-05-13 19:41:46 +0530

    Fix CORS

--- src/main/java/com/major/cookbook/config/SecurityConfig.java
+++ src/main/java/com/major/cookbook/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.major.cookbook.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import com.major.cookbook.security.JwtAuthenticationEntryPoint;
 import com.major.cookbook.security.JwtAuthenticationFilter;
@@ -33,19 +36,27 @@ public class SecurityConfig{
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http
-        	.csrf(csrf -> csrf.disable())
-            .cors(cors->cors.disable())
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8090"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+                return config;
+            }))
             .authorizeHttpRequests(
-            		auth->auth
-            		.requestMatchers("/api/auth/login").permitAll().requestMatchers("/api/auth/register").permitAll()
-            		.requestMatchers("/admin/api/**").hasRole("ADMIN")
-            		.requestMatchers("/api/**").hasRole("USER")
-            		.anyRequest().authenticated())
-            .exceptionHandling(ex->ex.authenticationEntryPoint(point))
-            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//Stateless since not stored in server
-            http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-            return http.build();
+                auth -> auth
+                    .requestMatchers("/api/auth/login").permitAll()
+                    .requestMatchers("/api/auth/register").permitAll()
+                    .requestMatchers("/admin/api/**").hasRole("ADMIN")
+                    .requestMatchers("/api/**").hasRole("USER")
+                    .anyRequest().authenticated())
+            .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Stateless since not stored in server
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
     
     @Bean

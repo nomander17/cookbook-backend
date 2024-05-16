b5eddb55b0af3283592616dfe8f1d657102ffeb5
Noman <nomander17@gmail.com>
2024-05-16 19:40:20 +0530

    /is-admin endpoint

--- src/main/java/com/major/cookbook/controller/AuthController.java
+++ src/main/java/com/major/cookbook/controller/AuthController.java
@@ -11,10 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -126,5 +128,13 @@ public class AuthController {
         } else {
             return ResponseEntity.badRequest().body("Invalid OTP. Please try again.");
         }
+    }
+
+    // checking if the user as the admin role
+    @GetMapping("/is-admin")
+    public ResponseEntity<Boolean> isAdmin(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userService.getUserByUsername(userDetails.getUsername());
+        return ResponseEntity.ok(user.getIsAdmin());
     }
 }
